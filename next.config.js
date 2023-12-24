97f9e8196944796c6a875d1b6d901d5b245573a9
piotrokrutniak <91792866+piotrokrutniak@users.noreply.github.com>
2023-12-24 14:19:16 +0100

    Updating eslint

--- next.config.js
+++ next.config.js
@@ -1,10 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images:{
+    images: {
         domains: [
             "via.placeholder.com"
         ]
-    }
+    },
+    eslint: { 
+        ignoreDuringBuilds: true, 
+    }, 
 }
 
 module.exports = nextConfig
