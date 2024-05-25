7f5474fc091ddf6cd93d317c8295239b78b4cfb9
Piotr Okrutniak <piotrokrutniak@gmail.com>
2024-05-25 17:09:20 +0200

    Toggle button export fix

--- src/app/(pages)/admin/recipes/edit/[id]/page.tsx
+++ src/app/(pages)/admin/recipes/edit/[id]/page.tsx
@@ -21,6 +21,7 @@ import axios from "axios";
 import { QuillEditor } from "@/app/components/generic/quill/QuillEditor";
 import { PublishedToggle } from "../../page";
 import clsx from "clsx";
+import { SimpleToggle } from "@/app/components/generic/toggleButton";
 
 export default function AddRecipePage({ params }: { params: { id: string } }) {
   const [recipe, setRecipe] = useState<RecipeType>({
@@ -337,21 +338,3 @@ async function uploadImageToCloudinary(file: File) {
     throw new Error("Failed to upload image");
   }
 }
-
-export const SimpleToggle = ({ initialValue, updateMethod }: { initialValue: boolean, updateMethod: (value: boolean) => void }) => {
-  const [value, setValue] = useState<boolean | undefined>(undefined);
-  const handleToggle = () => {
-    const newValue = !value;
-    setValue(newValue);
-    updateMethod(newValue);
-  };  
-  
-  return (
-    <button onClick={handleToggle} className="bg-slate-700 w-full max-w-[48px] p-2 rounded-lg relative text-white">
-      <div className={clsx([
-        "w-6 h-6 rounded-full absolute transition-transform duration-300 ease-in-out -top-[4px]",
-        value ?? initialValue ? "bg-indigo-500 right-0" : "bg-slate-400 left-0",
-      ])} />
-    </button>
-  );
-};
